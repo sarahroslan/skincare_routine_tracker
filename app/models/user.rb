1fe4879819f631f1c7c7a41447a23ac12664a4d5
Sarah <sarahroslan@gmail.com>
2018-12-14 19:04:42 +0800

    Added user validity test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
-  
+  def change
+   create_table :users do |t|
+
+      t.string :username
+      t.srting :email
+
+      t.timestamp
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,15 @@ ActiveRecord::Schema.define(version: 2018_12_14_021714) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "username", null: false
     t.string "email", null: false
     t.string "encrypted_password", limit: 128, null: false
     t.string "confirmation_token", limit: 128
     t.string "remember_token", limit: 128, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
 end
