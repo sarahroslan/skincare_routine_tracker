c7f7354d79037635e98753b735b11275f9bebdf3
Sarah <sarahroslan@gmail.com>
2019-01-04 12:14:59 +0800

    Added signup feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get 'welcome/index'
+
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
+
+  get "/sign_up" => "users#new", as: "sign_up"
 end
