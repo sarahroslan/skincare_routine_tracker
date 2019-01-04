10d207ccc3938f9ce1ce680abbe1bc94b2e33588
Sarah <sarahroslan@gmail.com>
2019-01-04 15:03:12 +0800

    Added sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
+ 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 
   resources :users
 
   get "/sign_up" => "users#new", as: "sign_up"
+  get "/sign_in" => "sessions#new", as: "sign_in"
+  post "/sign_in" => "sessions#create", as:"signed_in"
+  delete "/sign_out" => "sessions#destroy", as: "sign_out"
 end
